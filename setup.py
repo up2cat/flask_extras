47d6c18369fa871d8d517268d6bb9fd663215f97
alex <up2cat@gmail.com>
2020-01-08 23:57:52 +0200

    Update version.

--- setup.py
+++ setup.py
@@ -25,7 +25,7 @@ def readme():
 
 setup(
     name='flask_extras',
-    version='5.0.0',
+    version='5.0.1',
     description=('Assorted useful flask views, blueprints, '
                  'Jinja2 template filters, and templates/macros'),
     long_description=readme(),
